e410ebdfc07d868270d0bb0fd7cae896e6ad6f28
Daliane <daliane.rabelo01@gmail.com>
2025-08-14 15:42:31 -0300

    configura teste

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Vida Protegida')
+  .setDescription('Projeto Vida Protegida')
+  .setContact("TechGen7","http://www.","@email.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
